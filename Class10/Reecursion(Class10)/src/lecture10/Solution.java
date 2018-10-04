58c2a2a7fee82e882ac251b27b0610bf77eb2f06
SudokuLover <gauranggoel.9@gmail.com>
2018-10-04 14:24:01 +0530

    class 10

---
+++ Class10/Reecursion(Class10)/src/lecture10/Solution.java
@@ -0,0 +1,44 @@
+package lecture10;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String input = s.nextLine();
+		String output[] = subsequence(input);
+		for(int i = 0; i < output.length; i++) {
+			System.out.println(output[i]);
+		}
+	}
+	// Return the subsequences saved in a string array
+	public static String[] subsequence(String input){
+		// Write your code here
+
+      if(input.length()==0)
+      {
+        String str[]= {" "};
+      	return str;
+      }
+      
+      String []args=subsequence(input.substring(1));
+      
+      String []arr=new String[args.length*2];
+      
+      int i=0;
+      for(;i<args.length;i++)
+      {
+        arr[i]=args[i];
+      }
+      
+      int k=0;
+      for(;i<arr.length;i++)
+      {
+        arr[i]=input.charAt(0)+args[k++];
+      }
+      
+      return arr;
+	}
+}
+
---
+++ Class10/Reecursion(Class10)/src/lecture10/Solution2.java
@@ -0,0 +1,66 @@
+package lecture10;
+
+import java.util.Scanner;
+
+public class Solution2 {
+
+	// Return a 2D array that contains all the subsets
+	public static int[][] subsets(int input[]) {
+		// Write your code here
+      return allSubsets(input,0);
+
+	}
+  
+  public static int[][] allSubsets(int input[],int index){
+    if(index==input.length)
+    {
+      int [][]retval={{}};
+      return retval;
+    }
+    else{
+      int row[][]=allSubsets(input,index+1);
+      int arr[][]=new int[row.length*2][];
+      int i=0;
+      for(;i<row.length;i++)
+      {
+        arr[i]=new int[row[i].length];
+       	for(int j=0;j<row[i].length;j++)
+          arr[i][j]=row[i][j];
+      }
+      
+      int k=0;
+      for(;i<arr.length;i++)
+      {
+     	arr[i]=new int[row[k].length+1];
+        arr[i][0]=input[index];
+       	for(int j=1;j<=row[k].length;j++)
+          {
+        	arr[i][j]=row[k][j-1];
+          }
+       	k++;
+      }
+      return arr;
+    }
+    
+  }
+  public static int[] takeInput() {
+		Scanner s = new Scanner(System.in);
+		int size = s.nextInt();
+		int arr[] = new int[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = s.nextInt();
+		}
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		int[] input = takeInput();
+		int output[][] = subsets(input);
+		for(int i = 0; i < output.length; i++) {
+			for(int j = 0; j < output[i].length; j++) {
+				System.out.print(output[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
