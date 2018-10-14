1f62ee2612be997ea5ff18e99f360cc1576491d0
Gaurang <Gauranggoel.9@gmail.com>
2018-10-14 16:09:04 +0530

    Class 15

--- Class15/Oops(Class15)/src/lecture15/Bed.java
+++ Class15/Oops(Class15)/src/lecture15/Bed.java
@@ -1,5 +1,25 @@
 package lecture15;
 
-public class Bed {
-
+public class Bed extends Throwable{
+	private int length;
+	//int length;
+	int breadth;
+	int heigth;
+	int price;
+	String color;
+	
+	
+	/*public String toString(){
+		return ""+length+" "+breadth+" "+heigth+" "+price+" "+color;
+	}
+	*/
+	public int getLength(){
+		return length;
+	}
+	public void setLength(int len) throws Exception{
+		length=len;
+		if(len<0)
+			throw new Exception("Less than 10");
+			
+	}
 }
--- Class15/Oops(Class15)/src/lecture15/Client.java
+++ Class15/Oops(Class15)/src/lecture15/Client.java
@@ -4,6 +4,28 @@ public class Client {
 	
 	public static void main(String agrs[])
 	{
+		Bed b=new Bed();
+
+		b.heigth=2;
+		b.breadth=2;
+		//b.length=2;
+		try {
+			b.setLength(-9);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		b.price=20000;
+		b.color="White";
+		System.out.println(b);
+		//System.out.println(b.length);
+		System.out.println(b.getLength());
+		
+		Bed b1=new Bed();
+
+		System.out.println(b1);
+		//System.out.println(b1.length);
 		
 	}
 }
