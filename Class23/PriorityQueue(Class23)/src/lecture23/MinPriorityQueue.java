5903029de6ebbc18ad5e55a5ebe4e79e1a4f03dd
Gaurang <Gauranggoel.9@gmail.com>
2018-11-11 12:34:01 +0530

    class23

---
+++ Class23/PriorityQueue(Class23)/src/lecture23/MinPriorityQueue.java
@@ -0,0 +1,94 @@
+package lecture23;
+import java.util.ArrayList;
+public class MinPriorityQueue {
+
+	private  ArrayList<Integer> heap;
+	
+	public MinPriorityQueue(){
+		heap= new ArrayList<>();
+	}
+	public boolean isEmpty()
+	{
+		return heap.size()==0;
+	}
+	public void insert(int data){
+		heap.add(data);
+		insertHeapify(heap.size()-1);
+	}
+	public int delete() throws PriorityQueueException {
+		if(isEmpty())
+		{
+			throw new PriorityQueueException("no data");
+		} 
+		int data=heap.get(0);
+		heap.add(0,heap.get(heap.size()-1));
+		heap.remove(heap.size()-1);
+		if(heap.size()>0)
+			deleteHeapify(0);
+		return data;
+	}
+
+	private void insertHeapify(int index){
+		if(index==0)
+			return ;
+		
+		int temp=(index-1)/2;
+		
+		if(heap.get(temp)>heap.get(index))
+		{
+			swap(temp,index);
+			insertHeapify(temp);
+		}
+	}
+	public int getMin() throws PriorityQueueException{
+		if(isEmpty())
+		{
+			throw new PriorityQueueException("no data");
+		}
+		
+		return heap.get(0);
+	}
+	
+	private void deleteHeapify(int index){
+		
+		int leftChildIndex=2*index+1;
+		int rightChildIndex=2*index+2;
+		
+
+		if(leftChildIndex<heap.size()-1)
+			return;
+		//if(rightChildIndex<heap.size()-1)
+			//return;
+		
+		int minIndex=index;
+		if(heap.get(leftChildIndex)<heap.get(minIndex))
+			minIndex=leftChildIndex;
+		if((rightChildIndex<heap.size()-1) && heap.get(rightChildIndex)<heap.get(minIndex))
+			minIndex=rightChildIndex;
+		
+		if(minIndex!=index)
+		{
+			swap(index,minIndex);
+			
+			deleteHeapify(minIndex);
+		}
+		return ;
+
+		
+	}
+	private void swap(int i,int j){
+		int data=heap.get(i);
+		heap.set(i,heap.get(j));
+		heap.set(j,data);		
+	}
+}
+class PriorityQueueException extends Exception{
+	String s;
+	
+	public PriorityQueueException(String s){
+		this.s=s;
+	}
+	public String toString(){
+		return s;
+	}
+}
---
+++ Class23/PriorityQueue(Class23)/src/lecture23/MinPriorityQueueByMaam.java
@@ -0,0 +1,97 @@
+package lecture23;
+import java.util.ArrayList;
+
+public class MinPriorityQueueByMaam {
+private  ArrayList<Integer> heap;
+	
+	public MinPriorityQueueByMaam(){
+		heap= new ArrayList<>();
+	}
+	public boolean isEmpty()
+	{
+		return heap.size()==0;
+	}
+	public void insert(int data){
+		heap.add(data);
+		int  childIndex=heap.size()-1;
+		int parentIndex=(childIndex-1)/2;
+		
+		while(childIndex>0){
+			if(heap.get(childIndex)<heap.get(parentIndex))
+			{
+				swap(childIndex,parentIndex);
+				//int temp=heap.get(childIndex);
+				//heap.set(childIndex,heap.get(parentIndex));
+				//heap.set(parentIndex,temp);		
+				childIndex=parentIndex;
+				parentIndex=(childIndex-1)/2;
+			}
+			else{
+				break;
+			}
+			
+		}
+	}
+	public int getMin() throws PriorityQueueException{
+		if(isEmpty())
+		{
+			throw new PriorityQueueException("no data");
+		}
+		return heap.get(0);
+	}
+	
+	public int removeMin() throws PriorityQueueException{
+		if(isEmpty())
+		{
+			throw new PriorityQueueException("no data");
+		}
+		int data=heap.get(0);
+		heap.set(0, heap.get(heap.size()-1));
+		heap.remove(heap.size()-1);
+		
+		if(heap.size()>1)
+		{
+			downHeapify(0);
+		}
+		
+		return data;
+	}
+	private void downHeapify(int i){
+		
+		int leftChildIndex =2 * i +1;
+		int rightChildIndex =2 * i +2;
+
+		if(leftChildIndex<heap.size()-1)
+			return;
+		
+		int minIndex=i;
+		if(heap.get(leftChildIndex)<heap.get(minIndex))
+			minIndex=leftChildIndex;
+		if((rightChildIndex<heap.size()-1) && heap.get(rightChildIndex)<heap.get(minIndex))
+			minIndex=rightChildIndex;
+		
+		if(minIndex!=i)
+		{
+			swap(i,minIndex);
+			
+			downHeapify(minIndex);
+		}
+		return ;
+	}
+	private void swap(int i,int j){
+		int data=heap.get(i);
+		heap.set(i,heap.get(j));
+		heap.set(j,data);		
+	}
+	
+}
+class PriorityQueueException extends Exception{
+	String s;
+	
+	public PriorityQueueException(String s){
+		this.s=s;
+	}
+	public String toString(){
+		return s;
+	}
+}
