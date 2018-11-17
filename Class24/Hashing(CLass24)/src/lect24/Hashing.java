f8fa27f1d5c7c081283c10e9cc5bb9517c8b9a03
Gaurang <Gauranggoel.9@gmail.com>
2018-11-17 13:48:47 +0530

    class24

---
+++ Class24/Hashing(CLass24)/src/lect24/Hashing.java
@@ -0,0 +1,59 @@
+package lect24;
+
+import java.util.HashMap;
+import java.util.Scanner;
+class KeyValue{
+	int key;
+	int value;
+	public KeyValue(int k,int v){
+		key=k;
+		value=v;
+	}
+}
+public class Hashing {
+	
+	public static KeyValue maxFrequency(int arr[]){
+		int max=0;
+		int maxNumber=0;
+		
+		HashMap<Integer,Integer> map = new 	HashMap<>();
+		
+		for(int key:arr)
+		{
+			if(map.containsKey(key))
+				map.put(key, map.get(key)+1);
+			else
+				map.put(key, 1);		
+		}
+		for(int key:arr)
+		{
+				if(max< map.get(key)){
+					max= map.get(key);
+					maxNumber=key;
+				}
+		}
+		
+		return new KeyValue(maxNumber, max) ;
+	}
+	public static void main(String[] args) {
+		
+		/*
+		 HashMap<Integer,Integer> map = new 	HashMap<>();
+		map.put(1,2);
+		System.out.println(map.containsKey(1));
+		System.out.println(map.containsKey(2));
+		System.out.println(map.get(1));*/
+		
+		Scanner sc = new Scanner(System.in);
+	
+		int n = sc.nextInt();
+		int arr[]=new int[n];
+		
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		KeyValue p=maxFrequency(arr);
+		System.out.println("Key : "+p.key+" has value : "+p.value);
+	}
+}
